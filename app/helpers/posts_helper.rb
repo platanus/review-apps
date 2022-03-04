93ebb2c1cce6cd57e026c87e6c5c683d7fc97ca6
Ignacio Acosta <iaacosta@uc.cl>
2022-03-04 14:15:38 -0300

    feat(posts): add many stuff with scaffold

---
+++ app/helpers/posts_helper.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint(8)        not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+module PostsHelper
+end
