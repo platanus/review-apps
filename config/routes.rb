a7463674fc553a6d8ddee1215a7c45af5ac629a5
Ignacio Acosta <iaacosta@uc.cl>
2022-03-04 11:48:25 -0300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  mount Sidekiq::Web => '/queue'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  namespace :api, defaults: { format: :json } do
+    namespace :internal do
+    end
+  end
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
