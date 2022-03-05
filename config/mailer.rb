9c41fdd23cd81cced373faa976a32645c0280624
Ignacio Acosta <iaacosta@uc.cl>
2022-03-04 22:50:32 -0300

    chore(heroku): fix issue with mailers

--- config/mailer.rb
+++ config/mailer.rb
@@ -1,13 +1,20 @@
+APPLICATION_HOST =
+  if Rails.env.production? && Heroku.review_app?
+    "#{ENV.fetch('HEROKU_APP_NAME')}-pr-#{ENV.fetch('HEROKU_PR_NUMBER')}.herokuapp.com"
+  else
+    ENV.fetch('APPLICATION_HOST')
+  end
+
 Rails.application.config.action_mailer.raise_delivery_errors = false
 Rails.application.config.action_mailer.delivery_method = :aws_sdk
 
 Rails.application.config.action_mailer.default_url_options = {
-  host: ENV.fetch('APPLICATION_HOST'),
+  host: APPLICATION_HOST,
   protocol: 'https'
 }
 
 Rails.application.config.action_mailer.default_options = { from: ENV['DEFAULT_EMAIL_ADDRESS'] }
-ASSET_HOST = ENV.fetch("ASSET_HOST", ENV.fetch("APPLICATION_HOST"))
+ASSET_HOST = ENV.fetch("ASSET_HOST", APPLICATION_HOST)
 Rails.application.config.action_mailer.asset_host = ASSET_HOST
 
 if ENV["EMAIL_RECIPIENTS"].present?
